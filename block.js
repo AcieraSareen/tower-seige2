60232f11671cadaee1179c92662be41f65a55914
AcieraSareen <77666317+AcieraSareen@users.noreply.github.com>
2021-04-23 14:27:28 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,48 @@
+class Block{
+    constructor(x, y, width, height) {
+      this.Visibility=255;
+
+      this.image=loadImage("block(1).png");
+
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      
+      display(){
+        console.log(this.body.speed);
+        var pos=this.body.position;
+
+
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        if(this.body.speed<2){
+      
+          imageMode(CENTER);
+          image(this.image,this.body.position.x,this.body.position.y,50,50)
+      
+        }
+        else{
+          World.remove(world,this.body)
+          push();
+          this.visibility=this.visibility-5;
+          tint(255,this.visibility);
+          image(this.image,this.body.position.x,this.body.position.y,50,50)
+          pop();
+        }
+        
+        
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+     
+      
+}
